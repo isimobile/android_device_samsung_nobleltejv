ec4f3a87cf9fb7d7d20779f578cf6ec1c064b413
isimobile <corrie.meyer@gmail.com>
2016-10-21 15:16:36 +0200

    Initial commit

---
+++ cm.mk
@@ -0,0 +1,25 @@
+# Inherit device configuration
+$(call inherit-product, device/samsung/nobleltejv/nobleltejv.mk)
+$(call inherit-product, device/samsung/noblelte-common/noblelte.mk)
+
+# Inherit from the 64 bit configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit nfc enhanced configuration
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := cm_nobleltejv
+PRODUCT_DEVICE := nobleltejv
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-N920C
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=nobleltejv
---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+add_lunch_combo cm_nobleltejv-eng
+add_lunch_combo cm_nobleltejv-userdebug
